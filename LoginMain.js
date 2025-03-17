37c719cf972eec7f787f9ab5671a0b7bed328402
David24122005 <117317263+David24122005@users.noreply.github.com>
2025-03-16 17:58:47 -0700

    Add files via upload

---
+++ LoginMain.js
@@ -0,0 +1,9 @@
+// hola esto es un comentario hecho con ctrl + }
+// Hola david como estas, yo estoy bien
+
+function capturar() {
+  let user = document.getElementById("user").value;
+  let password = document.getElementById("password").value;
+  alert(user + " " + password);
+}
+// nose que estoy haciendo ando viendo un venezolano y habla bien chistoso
